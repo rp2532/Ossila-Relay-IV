f49f3b493a99ef0bdcf77b132fe909b052394c87
Rohit Prasanna <rohit.prasanna@gmail.com>
2019-12-05 17:57:07 -0800

    Minor changes

--- IVprogram.py
+++ IVprogram.py
@@ -3,6 +3,7 @@ ipaddress_ossila = '192.168.1.203'
 # Clear previously generated pyc files
 import os
 os.system('rm -f ./__pycache__/*')
+
 from mainprogram import *
 
 iv = IVMeasurement(ipaddress_ossila)
