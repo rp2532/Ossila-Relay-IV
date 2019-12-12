ae783596ea7689778e999c7a8b0a7cacaa10ad72
rp2532 <rohit.prasanna@gmail.com>
2019-12-12 19:42:58 +0100

    Modified readme with instructions, deleted an old file

--- IVprogram.py
+++
@@ -1,9 +0,0 @@
-ipaddress_ossila = '192.168.1.203'
-
-# Clear previously generated pyc files
-import os
-os.system('rmdir -f ./__pycache__/*')
-
-from mainprogram import *
-
-iv = IVMeasurement(ipaddress_ossila, 'COM6')
